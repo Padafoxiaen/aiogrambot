67a94a7b11c6ec6841d4e160eeea82d728a88dc3
Lowton05 <poka.tak.45@gmail.com>
2024-10-06 13:38:15 +0200

    Remade run.py and improve setup_locales function

--- run.py
+++ run.py
@@ -7,22 +7,13 @@ import betterlogging as logging  # type: ignore
 
 from bot import bot, dp, router
 from bot.middlewares import middlewares_list
-from service.exception_messages import locale_not_exist
 from service.utils import setup_locales, setup_middlewares
 
 
-async def main(
-        locales: list[str] | None,
-        init_locales: bool = False
-) -> None:
+async def main() -> None:
 
     # Check if developer wants to just update locales
-    if init_locales:
-        if locales:
-            setup_locales(locales)
-            return
-        else:
-            raise ValueError(locale_not_exist)
+    setup_locales(switch=False, locales=['en', 'uk', 'de', 'jp'])
 
     # Other bot settings
     await bot.delete_webhook(drop_pending_updates=True)
@@ -37,5 +28,4 @@ async def main(
 if __name__ == '__main__':
     # Setup logging
     logging.basic_colorized_config(level=logging.INFO)
-
-    asyncio.run(main(init_locales=False, locales=['en', 'uk', 'de', 'jp']))
+    asyncio.run(main())
--- service/utils.py
+++ service/utils.py
@@ -7,8 +7,10 @@ import betterlogging as logging  # type: ignore
 from aiogram import Dispatcher
 from aiogram_i18n.middleware import I18nMiddleware
 
+from service.exception_messages import locale_not_exist
 
-def setup_locales(locales: list[str]) -> None:
+
+def setup_locales(switch: bool, locales: list[str]) -> None:
     """
     Function, that simulated a request to aiogram_i18n cli 
     to setup all locales
@@ -17,26 +19,31 @@ def setup_locales(locales: list[str]) -> None:
         - locales: list[str]: A list with all names from locales folder that was
         created in 'bot/locales' folder
     """
-    cmd = [
-        'python', '-m', 'aiogram_i18n', 'multiple-extract', '-i',
-        './bot/', '-o', './bot/locales/'
-    ]
 
-    for locale in locales:
-        cmd.extend(['--locales', locale])
+    if switch:
+        if locales:
+            cmd = [
+                'python', '-m', 'aiogram_i18n', 'multiple-extract', '-i',
+                './bot/', '-o', './bot/locales/'
+            ]
 
-    # Print final command
-    logging.info(f"Executing: {' '.join(cmd)}")
+            for locale in locales:
+                cmd.extend(['--locales', locale])
 
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    if result.returncode != 0:
-        logging.error(result.stderr)
-        logging.error(
-            f"Command failed with error code {result.returncode}"
-        )
-    else:
-        logging.info(result.stdout)
-        logging.info("Command executed successfully")
+            # Print final command
+            logging.info(f"Executing: {' '.join(cmd)}")
+
+            result = subprocess.run(cmd, capture_output=True, text=True)
+            if result.returncode != 0:
+                logging.error(result.stderr)
+                logging.error(
+                    f"Command failed with error code {result.returncode}"
+                )
+            else:
+                logging.info(result.stdout)
+                logging.info("Command executed successfully")
+        else:
+            raise ValueError(locale_not_exist)
 
 
 def setup_middlewares(
