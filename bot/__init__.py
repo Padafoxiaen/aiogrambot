748176bfaa43f14501d37dad539e2d05f64a3f2b
Lowton05 <poka.tak.45@gmail.com>
2024-10-11 00:07:18 +0200

    Added transporter which stores all project data

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,31 +2,24 @@ from aiogram import Bot, Dispatcher, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.redis import RedisStorage
-from environs import Env
 
-# Creating an own basic environment variable
-basic_env = Env()
-basic_env.read_env('.env')
+from data import transporter as t
 
 # Creating a bot instance
-bot_token = basic_env.str('BOT_TOKEN')
+bot_token = t.bot.BOT_TOKEN
 if bot_token:
     bot = Bot(
         token=bot_token,
         default=DefaultBotProperties(parse_mode='HTML')
     )
 else:
-    msg = 'Did you set bot token in your environments variable?'
+    msg = 'Did you set a bot token in your environments variable?'
     raise ValueError(msg)
 
 # Creating other important instances
-redis_password = basic_env.str('REDIS_PASSWORD')
-redis_host = basic_env.str('REDIS_HOSTNAME')
-redis_port = basic_env.int('REDIS_PORT')
-
-
 dp = Dispatcher(storage=RedisStorage.from_url(
-    f'redis://default:{redis_password}@{redis_host}:{redis_port}/0'
+    f'redis://default:{t.redis.REDIS_PASSWORD}'
+    f'@{t.redis.REDIS_IP}:{t.redis.REDIS_PORT}/{t.redis.REDIS_DATABASE}'
 ))
 
 router = Router()
--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,43 @@
-from sqlalchemy import create_engine
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings
 
-# Create engine: starting point to database connection
-engine = create_engine(
-    url='url'
-)
+
+class ForEnvConfig(BaseSettings):
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+class BotSettings(ForEnvConfig):
+    """
+    Settings of telegram bot
+    """
+
+    BOT_TOKEN: str = Field(..., description='Telegram bot token')
+
+
+class RedisSettings(ForEnvConfig):
+    REDIS_IP: str = Field(...)
+    REDIS_PORT: int = Field(..., default=6379)
+    REDIS_DATABASE: int = Field(..., default=0)
+    REDIS_PASSWORD: str = Field(...)
+
+
+class PostgresSettings(ForEnvConfig):
+    POSTGRES_USER: str = Field(...)
+    POSTGRES_PORT: int = Field(..., default=5432)
+    POSTGRES_IP: str = Field(...)
+    POSTGRES_PASSWORD: str = Field(...)
+    POSTGRES_DATABASE: str = Field(..., default='Main')
+
+
+class ProjectStorage(BaseModel):
+    """
+    Here are stored data from all parts of this project
+    """
+    bot: BotSettings = BotSettings()
+    redis: RedisSettings = RedisSettings()
+    postgres: PostgresSettings = PostgresSettings()
+
+
+transporter = ProjectStorage()
